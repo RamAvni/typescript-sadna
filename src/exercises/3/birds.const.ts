ee1acff492d26fbca3dad691535736db151ab1d7
RamAvni <ramavni@hilma.tech>
2025-12-02 22:38:44 +0200

    fixd exercise 3

--- src/exercises/3/birds.const.ts
+++ src/exercises/3/birds.const.ts
@@ -1,28 +1,28 @@
-import { WaterBird } from "./types/waterBird.interface";
+import { Duck } from "./types";
 
-export const birds: WaterBird[] = [
-  {
-    cuteName: "Hemda Imber",
-    featherColor: "gray",
-    age: 215,
-    canSwim: false,
-  },
-  {
-    cuteName: "Dor Greenwald",
-    featherColor: "green",
-    age: 2,
-    canSwim: false,
-  },
-  {
-    cuteName: "Tikva Imber",
-    featherColor: "orange",
-    age: 9.5,
-    canSwim: true,
-  },
-  {
-    cuteName: "Adi Koifman",
-    featherColor: "white",
-    age: 4,
-    canSwim: true,
-  },
-];
+// export const birds: Duck[] = [
+//   {
+//     cuteName: "Hemda Imber",
+//     featherColor: "gray",
+//     age: 215,
+//     canSwim: false,
+//   },
+//   {
+//     cuteName: "Dor Greenwald",
+//     featherColor: "green",
+//     age: 2,
+//     canSwim: false,
+//   },
+//   {
+//     cuteName: "Tikva Imber",
+//     featherColor: "orange",
+//     age: 9.5,
+//     canSwim: true,
+//   },
+//   {
+//     cuteName: "Adi Koifman",
+//     featherColor: "white",
+//     age: 4,
+//     canSwim: true,
+//   },
+// ];
